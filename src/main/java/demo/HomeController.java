00a143a4c47014f5dd116bd9292fbd4fc8d6167c
Edgar Gonzalez <edggonzalezg@gmail.com>
2015-09-17 20:12:42 -0400

    Move index to home

--- src/main/java/demo/HomeController.java
+++ src/main/java/demo/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String index() {
-        return "index";
+        return "home";
     }
 
     @RequestMapping(value = "/secure", method = RequestMethod.GET)
